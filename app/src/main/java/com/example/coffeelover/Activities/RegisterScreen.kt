7119825eddaca463ccd7d5531f74d9944e135307
Dương <leduong.w01@gmail.com>
2023-04-15 18:22:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/coffeelover/Activities/RegisterScreen.kt
@@ -0,0 +1,12 @@
+package com.example.coffeelover.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coffeelover.R
+
+class RegisterScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen)
+    }
+}
