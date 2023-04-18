55d2c523c4514111ebbc0241124af453afc32835
Dương <leduong.w01@gmail.com>
2023-04-18 16:38:46 +0700

    chỉnh lại logo và tạo intent
    + Chỉnh lại logo Google
    + thêm intent đăng nhâp và đăng ký ở các màn StartScreen và Login/RegisterScreen

--- app/src/main/java/com/example/coffeelover/Activities/LoginScreen.kt
+++ app/src/main/java/com/example/coffeelover/Activities/LoginScreen.kt
@@ -1,11 +1,46 @@
 package com.example.coffeelover.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.coffeelover.R
+import kotlinx.android.synthetic.main.activity_login_screen.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
     }
+
+    fun genEvent(){
+        // click cua dang nhap
+        bt_login.setOnClickListener {
+            //check
+
+            loginProcess()
+        }
+    }
+    fun gotoRegister(){
+        val registerIntent = Intent(this, RegisterScreen::class.java)
+        startActivity(registerIntent)
+        this.finish()
+    }
+    fun loginProcess(){
+        //login process
+
+        //intent
+//        val homeIntent = Intent(this, HomeScreen::class.java)
+//        startActivity(homeIntent)
+//        this.finish()
+    }
+//  unused
+//    fun gotoForgotPassword(){
+//        val registerIntent = Intent(this, RegisterScreen::class.java)
+//        startActivity(registerIntent)
+//        this.finish()
+//    }
+
 }
--- app/src/main/java/com/example/coffeelover/Activities/StartScreen.kt
+++ app/src/main/java/com/example/coffeelover/Activities/StartScreen.kt
@@ -1,11 +1,34 @@
 package com.example.coffeelover.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coffeelover.R
+import kotlinx.android.synthetic.main.activity_start_screen.*
 
 class StartScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_screen)
+        genEvent()
+    }
+
+    fun genEvent(){
+        bt_login.setOnClickListener {
+            gotoLogin()
+        }
+        bt_register.setOnClickListener{
+            gotoRegister()
+        }
+    }
+    private fun gotoLogin(){
+        val intentLogin = Intent(this, LoginScreen::class.java)
+        startActivity(intentLogin)
+        this.finish()
+    }
+    private fun gotoRegister(){
+        val intentRegister = Intent(this, RegisterScreen::class.java)
+        startActivity(intentRegister)
+        this.finish()
     }
 }
